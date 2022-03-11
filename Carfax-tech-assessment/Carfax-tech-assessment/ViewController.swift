e994d6f123debc599cb8ec8d9254b9cd2d8ec50b
Al <alex.shillingford@icloud.com>
2022-03-10 17:41:22 -0700

    Create Initial Project with .gitignore file

---
+++ Carfax-tech-assessment/Carfax-tech-assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carfax-tech-assessment
+//
+//  Created by William Shillingford on 3/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
